c833c1e76cc533d50be179f31ac00cfb883e376a
SerhiiM <msm72@mail.ru>
2017-02-03 18:23:58 +0200

    add func draw single line with color for CustomView

---
+++ OmnieCommerceAdmin/Common/UIViews/CustomView.swift
@@ -0,0 +1,47 @@
+//
+//  CustomView.swift
+//  OmnieCommerceAdmin
+//
+//  Created by msm72 on 03.02.17.
+//  Copyright © 2017 Omniesoft. All rights reserved.
+//
+
+import UIKit
+
+enum ViewDesign: String {
+    case SingleBottomVeryLightGrayLine = "SingleBottomVeryLightGrayLine"
+}
+
+@IBDesignable class CustomView: UIView {
+    // MARK: - Properties
+    var style: ViewDesign!
+    
+    @IBInspectable var viewDesign: String? {
+        set {
+            if let designName = newValue, let viewDesign = ViewDesign(rawValue: designName) {
+                style = viewDesign
+                setNeedsDisplay()
+            }
+        }
+        
+        get { return nil }
+    }
+    
+    
+    // MARK: - Class Functions
+    override func draw(_ rect: CGRect) {
+        switch style! {
+        case .SingleBottomVeryLightGrayLine:
+            let drawPath = UIBezierPath()
+            
+            drawPath.move(to: CGPoint.init(x: bounds.minX, y: bounds.maxY))
+            drawPath.addLine(to: CGPoint.init(x: bounds.maxX, y: bounds.maxY))
+            
+            drawPath.lineWidth = 1.0
+            let drawPathColor = UIColor(hexString: "#dedede", withAlpha: 1.0)!
+            drawPathColor.setStroke()
+
+            drawPath.stroke()
+        }
+    }
+}
